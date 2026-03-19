a91ee9c2e8288d44c90a33be22ff8a8102eae37b
Kiaraw <kiara.wurtz@epitech.digital>
2026-03-19 15:18:47 +0100

    ADD, contract

---
+++ frontend/lib/contract.ts
@@ -0,0 +1,35 @@
+export const CONTRACT_ADDRESS = "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+
+export const CONTRACT_ABI = [
+  {
+    "inputs": [
+      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "string", "name": "_serialNumber", "type": "string" },
+      { "internalType": "string", "name": "_model", "type": "string" },
+      { "internalType": "string", "name": "_metadataURI", "type": "string" }
+    ],
+    "name": "mintWatch",
+    "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "getWatch",
+    "outputs": [
+      { "internalType": "string", "name": "", "type": "string" },
+      { "internalType": "string", "name": "", "type": "string" },
+      { "internalType": "string", "name": "", "type": "string" },
+      { "internalType": "address", "name": "", "type": "address" }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{ "internalType": "uint256", "name": "tokenId", "type": "uint256" }],
+    "name": "ownerOf",
+    "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const
